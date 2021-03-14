efcce4b27de6609316f24651767e1b3a09bbca78
tartie sorel <sorelaugustin@gmail.com>
2021-03-14 20:28:22 +0000

    double pendulum added

--- DoublePendulum/MainWindow.xaml.cs
+++ DoublePendulum/MainWindow.xaml.cs
@@ -11,10 +11,10 @@ namespace DoublePendulum
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double r1 = 100;
-        private double r2 = 100;
-        private double m1 = 10;
-        private double m2 = 10;
+        private double r1 = 200;
+        private double r2 = 200;
+        private double m1 = 40;
+        private double m2 = 40;
         private double a1 = 0;
         private double a2 = 0;
 
@@ -29,13 +29,25 @@ namespace DoublePendulum
             double x1 = r1 * Math.Sin(a1);
             double y1 = r1 * Math.Cos(a1);
 
-            mainArm.X1 = 0 + 300;
-            mainArm.Y1 = 0 + 50;
+            double x2 = x1 + r2 * Math.Sin(a2);
+            double y2 = y1 + r2 * Math.Cos(a2);
+
+            mainArm.X1 = 300;
+            mainArm.Y1 = 50;
             mainArm.X2 = x1 + 300;
             mainArm.Y2 = y1 + 50;
 
-            center.Center = new Point(x1+300, y1+50);
+            center.Center = new Point(x1 + 300, y1 + 50);
             center.RadiusX = center.RadiusY = m1;
+
+
+            secondArm.X1 = x1 + 300;
+            secondArm.Y1 = y1 + 50;
+            secondArm.X2 = x2 + 300;
+            secondArm.Y2 = y2 + 50;
+
+            center2.Center = new Point(x2 + 300, y2 + 50);
+            center2.RadiusX = center2.RadiusY = m2;
         }
 
         #region Create Timer

f9eb2b83b49f928b6c3551591dc842f3995f3128
tartie sorel <sorelaugustin@gmail.com>
2021-03-15 20:48:44 +0000

    topbar logic added

--- DoublePendulum/GlobalColors.cs
+++ DoublePendulum/GlobalColors.cs
@@ -2,6 +2,7 @@
 {
     static class GlobalColors
     {
-        public static string BackgroundColor { get; set; } = "#FF2c3e50";
+        public static string StrongBackgroundColor { get; set; } = "#FF2c3e50";
+        public static string BackgroundColor { get; set; } = "#FF34495e";
     }
 }
--- DoublePendulum/Views/TopBarView/TopBarView.xaml.cs
+++ DoublePendulum/Views/TopBarView/TopBarView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DoublePendulum
 {
@@ -20,9 +8,53 @@ namespace DoublePendulum
     /// </summary>
     public partial class TopBarView : UserControl
     {
+        #region Buttons Const
+        private const byte DEFAULT_BUTTON_HEIGHT = 10;
+        private const byte ON_HOVER_BUTTON_HEIGHT = 13;
+        #endregion
+
         public TopBarView()
         {
             InitializeComponent();
+            AddMouseUpEventToEllipse();
+            AddDragAndDropEventToRectangle();
+            AddButtonCloseHoverEvent();
+            AddButtonMinimizeHoverEvent();
         }
+
+        #region Button Minimize Hover Event
+        private void AddButtonMinimizeHoverEvent()
+        {
+            ButtonMinimize.MouseEnter += (s, e) => ButtonMinimize.Width = ButtonMinimize.Height = ON_HOVER_BUTTON_HEIGHT;
+            ButtonMinimize.MouseLeave += (s, e) => ButtonMinimize.Width = ButtonMinimize.Height = DEFAULT_BUTTON_HEIGHT;
+        }
+        #endregion
+
+        #region Button Close Hover Event
+        private void AddButtonCloseHoverEvent()
+        {
+            ButtonClose.MouseEnter += (s, e) => ButtonClose.Width = ButtonClose.Height = ON_HOVER_BUTTON_HEIGHT;
+            ButtonClose.MouseLeave += (s, e) => ButtonClose.Width = ButtonClose.Height = DEFAULT_BUTTON_HEIGHT;
+        }
+        #endregion
+
+        #region Add Drag And Drop Event To Rectangle
+        private void AddDragAndDropEventToRectangle()
+        {
+            RectangleDragZone.MouseDown += (s, e) =>
+            {
+                if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+                    (Application.Current.Windows[0] as MainWindow).DragMove();
+            };
+        }
+        #endregion
+
+        #region Add Mouse Up Event To Ellipse
+        private void AddMouseUpEventToEllipse()
+        {
+            ButtonClose.MouseLeftButtonUp += (s, e) => Application.Current.Shutdown();
+            ButtonMinimize.MouseLeftButtonUp += (s, e) => (Application.Current.Windows[0] as MainWindow).WindowState = WindowState.Minimized;
+        }
+        #endregion
     }
 }

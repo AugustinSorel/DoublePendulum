3e4702ef29f40b54fc5c38ad147c4c058de745bc
tartie sorel <sorelaugustin@gmail.com>
2021-03-22 21:47:40 +0000

    foreground color added

--- DoublePendulum/Global/GlobalColors/GlobalColors.cs
+++ DoublePendulum/Global/GlobalColors/GlobalColors.cs
@@ -4,5 +4,6 @@
     {
         public static string StrongBackgroundColor { get; set; } = "Black";
         public static string BackgroundColor { get; set; } = "Black";
+        public static string ForegroundColor { get; set; } = "#FFe67e22";
     }
 }
